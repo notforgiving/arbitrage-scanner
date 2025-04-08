0db6f4d1c32ce85ae3d5b588be34ec1d7f508b68
notforgiving <notforgiving@list.ru>
2025-04-08 10:42:55 +0300

    ++

--- src/i18n/translations/en.ts
+++ src/i18n/translations/en.ts
@@ -19,6 +19,18 @@ export default {
             password: 'Password',
             confirm_pass: 'Password confirmation',
             register: 'Register',
+        },
+        how: {
+            title: 'How does our tool work?',
+            desc: 'Monitor the activity of large wallets, analyze the liquidity and automate the search for profitable trades in a few clicks',
+            item_title_1: 'Monitoring of on-chain activity',
+            item_text_1: 'Get instant access to capital movements on blockchains. We have the widest and most convenient tool on the market for analyzing blockchain data and wallets.',
+            item_title_2: 'Strategy analysis of major players',
+            item_text_2: 'Our service captures the actions of funds, market makers and experienced traders, helping to identify patterns and trends.',
+            item_title_3: 'Detecting early signals',
+            item_text_3: 'Identify promising projects and tokens at the stage of accumulation. Filter transactions by amount, frequency, and interaction with other wallets.',
+            item_title_4: 'Automated alerts',
+            item_text_4: 'Set up customized notifications and respond to important events in real time.',
         }
     }
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,8 @@ import React, { FC } from 'react';
 import Button from 'components/UI/Button';
 import { useHistory } from 'react-router-dom';
 import { Trans, useTranslation } from 'react-i18next';
+import SectionsGrid from 'components/SectionsGrid';
+import HowImg from 'assets/how.webp';
 import css from './styles.module.scss';
 
 const Home: FC = () => {
@@ -34,6 +36,35 @@ const Home: FC = () => {
               </ul>
             </div>
           </section>
+          <SectionsGrid>
+            <section className={css.how}>
+              <div className={css.section_title}>{t(`how.title`)}</div>
+              <div className={css.section_desc}>{t(`how.desc`)}</div>
+              <div className={css.how_info}>
+                <div className={css.how_items}>
+                  <div className={css.how_item}>
+                    <strong>{t(`how.item_title_1`)}</strong>
+                    <span>{t(`how.item_text_1`)}</span>
+                  </div>
+                  <div className={css.how_item}>
+                    <strong>{t(`how.item_title_2`)}</strong>
+                    <span>{t(`how.item_text_2`)}</span>
+                  </div>
+                </div>
+                <img src={HowImg} alt="" className={css.how_img} />
+                <div className={css.how_items}>
+                  <div className={css.how_item}>
+                    <strong>{t(`how.item_title_3`)}</strong>
+                    <span>{t(`how.item_text_3`)}</span>
+                  </div>
+                  <div className={css.how_item}>
+                    <strong>{t(`how.item_title_4`)}</strong>
+                    <span>{t(`how.item_text_4`)}</span>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </SectionsGrid>
         </div>
       </div>
     </>
