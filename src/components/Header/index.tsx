3ce66224ef863304661a27757c8dff6aca749146
notforgiving <notforgiving@list.ru>
2025-04-08 10:52:00 +0300

    ++

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import cn from 'classnames';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import Button from 'components/UI/Button';
 import ChangeLanguage from 'components/UI/ChangeLanguage';
 import { useTranslation } from 'react-i18next';
@@ -12,20 +12,26 @@ import i18n from '../../i18n';
 const Header: FC = () => {
   const { t } = useTranslation();
   const history = useHistory();
-  
+
   return (
     <div className={css.header}>
       <div className={cn('container', css.header_container)}>
-        <Link to="/">
-          <LogoSvg />
-        </Link>
+        <LogoSvg className={css.logo} />
         <div className={css.header_actions}>
           <ChangeLanguage />
           <div className={css.header_btns}>
-            <Button onClick={()=>history.push('/register')}>{t(`registrations`)}</Button>
-            <Button type="blue" onClick={()=>history.push('/auth')}>{i18n.t(`login`)}</Button>
+            <Button onClick={() => history.push('/register')}>
+              {t(`registrations`)}
+            </Button>
+            <Button type="blue" onClick={() => history.push('/auth')}>
+              {i18n.t(`login`)}
+            </Button>
           </div>
-          <FiLogIn className={css.header_login} size={20} onClick={()=>history.push('/auth')}/>
+          <FiLogIn
+            className={css.header_login}
+            size={20}
+            onClick={() => history.push('/auth')}
+          />
         </div>
       </div>
     </div>
