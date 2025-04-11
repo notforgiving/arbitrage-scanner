dfcaf56b1d112691a25a242003072c83576194f5
notforgiving <notforgiving@list.ru>
2025-04-11 21:05:52 +0300

    new video

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import './styles/main.scss';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
-
+ 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
